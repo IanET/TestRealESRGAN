617c505266cba7a333a2db396205dfc106c4c9f5
IanET <1154506+IanET@users.noreply.github.com>
2025-12-11 11:48:56 -1000

    Slow but image scaled

--- TestRealEsrgan.app.jl
+++ TestRealEsrgan.app.jl
@@ -47,51 +47,57 @@ status = CreateCpuMemoryInfo(ort, OrtArenaAllocator, OrtMemTypeDefault, memory_i
 check_status(ort, status)
 @info "CreateCpuMemoryInfo" status memory_info[]
 
-# See: https://github.com/quic/ai-hub-apps/tree/main/apps/android/SuperResolution
-# input: name = "image", shape = [1, 3, 128, 128], type = float32
-# output: name = "upscaled_image", shape = [1, 3, 512, 512], type = float32 
-
 img = load("Sample2.jpg") 
 @info "Input image shape:" size(img) # 128x128
 cv = channelview(img)
-cvf32 = convert(Array{Float32}, cv)
+cvf32 = Float32.(cv) # N0f8 is 0.0 - 1.0
+cvf32p = permutedims(cvf32, (2, 3, 1)) |> collect # 3x128x128 in row-major order
 
-# cvf32p = permutedims(cvf32, (2, 3, 1))
+@info "Input array shape:" size(cvf32p) 
 
-# input_shape = Clonglong[3, 4, 5]
+input_shape = Clonglong[1, 3, 128, 128]
 # input_values = fill(Cfloat(1.0), 5, 4, 3) # ONNX uses row-major order
-# input_tensor = Ptr{OrtValue}() |> Ref   
-# status = CreateTensorWithDataAsOrtValue(ort, memory_info[], input_values, sizeof(input_values), input_shape, length(input_shape), ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, input_tensor)
-# check_status(ort, status)
-# @info "CreateTensorWithDataAsOrtValue" status input_tensor[]
+input_tensor = Ptr{OrtValue}() |> Ref   
+status = CreateTensorWithDataAsOrtValue(ort, memory_info[], cvf32p, sizeof(cvf32p), input_shape, length(input_shape), ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, input_tensor)
+check_status(ort, status)
+@info "CreateTensorWithDataAsOrtValue" status input_tensor[]
 
-# input_tensors = [input_tensor[]]
-# output_tensor = Ptr{OrtValue}() 
-# output_tensors = [output_tensor]
-# num_outputs = length(output_tensors)
-# input_names = [pointer(INPUT_NAME)] # Needs GC preserve
-# output_names = [pointer(OUTPUT_NAME)] # Needs GC preserve
-# output_tensors = Ptr{OrtValue}() |> Ref
+input_tensors = [input_tensor[]]
+output_tensor = Ptr{OrtValue}() 
+output_tensors = [output_tensor]
+num_outputs = length(output_tensors)
+input_name = "image"
+output_name = "upscaled_image"
+input_names = [pointer(input_name)] # Needs GC preserve
+output_names = [pointer(output_name)] # Needs GC preserve
+output_tensors = Ptr{OrtValue}() |> Ref
 
-# @preserve INPUT_NAME OUTPUT_NAME begin 
-#     status = Run(ort, session[], C_NULL, input_names, input_tensors, length(input_tensors), output_names, length(output_names), output_tensors)
-# end
-# check_status(ort, status)   
-# @info "Run" status output_tensors[]
+@preserve input_name output_name begin 
+    status = Run(ort, session[], C_NULL, input_names, input_tensors, length(input_tensors), output_names, length(output_names), output_tensors)
+end
+check_status(ort, status)   
+@info "Run" status output_tensors[]
 
-# out = Ptr{Cvoid}() |> Ref
-# status = GetTensorMutableData(ort, output_tensors[], out)
-# check_status(ort, status)
-# @info "GetTensorMutableData" status out[]
+out = Ptr{Cvoid}() |> Ref
+status = GetTensorMutableData(ort, output_tensors[], out)
+check_status(ort, status)
+@info "GetTensorMutableData" status out[]
 
-# output_array = unsafe_wrap(Array, out[] |> Ptr{Cfloat}, prod(input_shape)) |> v -> reshape(v, (5, 4, 3))
-# @info "Output values:" output_array
-# @assert isapprox.(output_array, 0.731) |> all
+output_shape = Clonglong[1, 3, 512, 512]
+output_array = unsafe_wrap(Array, out[] |> Ptr{Cfloat}, prod(output_shape))
+
+# The output from ONNX is a flat, row-major array. Reshape it and convert it for Julia.
+# Just ignore the batch dimension (1).
+output_row_major = reshape(output_array, (512, 512, 3))
+output_col_major = permutedims(output_row_major, (3, 1, 2))
+output_n0f8 = N0f8.(clamp.(output_col_major, 0.0f0, 1.0f0))
+img_out = colorview(RGB, output_n0f8)
+# save("Sample2_upscaled.png", img_out)
+@info "Upscaled image saved to Sample2_upscaled.png"
 
 # Clean up resources
-# ReleaseValue(ort, input_tensor[])
-# ReleaseValue(ort, output_tensors[])
-
+ReleaseValue(ort, input_tensor[])
+ReleaseValue(ort, output_tensors[])
 ReleaseSession(ort, session[])
 ReleaseSessionOptions(ort, options[])
 ReleaseEnv(ort, env[])
